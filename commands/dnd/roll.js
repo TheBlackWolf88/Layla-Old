13e9558f32d96a0e6715bbbf65a0817548932c81
TheBlackWolf88 <mopszi88@gmail.com>
2022-10-28 10:18:24 +0200

    Bassza meg Toti

--- commands/dnd/roll.js
+++ commands/dnd/roll.js
@@ -12,7 +12,7 @@ module.exports = {
 		let NoD = 1
 		let sum=0
 		if(args[1]) NoD = Number(args[1])
-		if(NoD < 5){
+		if(NoD < 10){
 			for (let i = 0; i < NoD; i++) {
 				message.channel.send(`You rolled a D${dice}; you rolled a ${getRandomIntInclusive(1,dice)}`)
 			}
